b84809ba00058c404c24fbd6c4e09469214bc9b0
Jose Marcio <josemarciojunior10@gmail.com>
2026-02-24 12:39:21 -0300

    Finalizando o projeto e iniciando as chamadas das funções.

--- projeto_pib/etl_projeto_pib.py
+++ projeto_pib/etl_projeto_pib.py
@@ -1,10 +1,12 @@
 # importando bibliotecas necessárias
 
 import pandas as pd
+import numpy as np
 import glob
 import requests
 from bs4 import BeautifulSoup
 import sqlite3
+from datetime import datetime
 
 
 url = 'https://web.archive.org/web/20230824184940/https://pt.wikipedia.org/wiki/Lista_de_pa%C3%ADses_por_PIB_nominal'
@@ -20,11 +22,22 @@ csv_path = 'C:/Estudos Engenharia de Dados/projeto_pib/Paises_PIB.csv'
 # Criando função para extrair os dados
 
 def extrair(url, atributos_tabela):
+    #Faz requisição na página web
     pagina = requests.get(url).text
+   
+    # Crio variável para armazenar a leitura da página
     dados = BeautifulSoup(pagina, 'html.parser')
+    
+    # Crio um dataframe utilizando as colunas criadas com a variável de atributos da tabela
     df = pd.DataFrame(columns={atributos_tabela })
+    
+    # Busca da tag de tbody
     tabela = dados.find_all('tbody')
+    
+    # Busca as linhas da tabela
     rows = tabela[2].find_all('tr')
+    
+    # Itera sobre as linhas
     for row in rows:
         col = row.find_all('td')
         if len(col) != 0:
@@ -40,4 +53,31 @@ def extrair(url, atributos_tabela):
             break
     
 
-            
+def transformar(df):
+    lista_PIB = df["PIB"].tolist()
+    lista_PIB = [float("".join(x.split(','))) for x in lista_PIB]
+    lista_PIB = [np.round(x/1000, 2) for x in lista_PIB]
+    df["PIB"] = lista_PIB
+    df = df.rename(columns={"PIB": "PIB_Bilhoes"})
+    return df
+
+def carregar_csv(df, csv_path):
+    df.to_csv(csv_path)
+    
+def carregar_db(df, sql_connection, nome_db):
+    df.to_sql(nome_tabela, sql_connection, if_existas='replace', index=False)
+    
+def query(query_statement, sql_connection):
+    print(query_statement)
+    query_output = pd.read_sql(query_statement, sql_connection)
+    print(query_output)
+    
+def log_processos(mensagem):
+    timestamp_format = '%Y-%g-%d-%H:%M:%S'
+    now = datetime.now()
+    timestamp = now.strftime(timestamp_format)
+    with open("./projeto_pib/etl_projeto_log.txt") as f:
+        f.write(timestamp + ': ' + mensagem + '\n')
+        
+# Chamadas de função
+df = extrair(url, atributos_tabela)
