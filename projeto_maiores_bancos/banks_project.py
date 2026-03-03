538096006b0f9c0aa7b30faca655610dbfdad97a
Jose Marcio <josemarciojunior10@gmail.com>
2026-03-02 22:46:09 -0300

    Finalizando o projeto colocando os termos em ingês conforme o projeto apresentado no curso

--- projeto_maiores_bancos/banks_project.py
+++ projeto_maiores_bancos/banks_project.py
@@ -6,23 +6,23 @@ import numpy as np
 import sqlite3
 
 
-def extrair(url, atributos_tabela):
+def extract(url, table_attribs):
     # Adicionando um User-Agent para evitar bloqueios
     headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36'}
     
-    pagina = requests.get(url, headers=headers).text
-    dados = BeautifulSoup(pagina, 'html.parser')
-    df = pd.DataFrame(columns = atributos_tabela)
-    tabela = dados.find_all('tbody')
-    rows = tabela[2].find_all('tr')
+    page = requests.get(url, headers=headers).text
+    data = BeautifulSoup(page, 'html.parser')
+    df = pd.DataFrame(columns = table_attribs)
+    table = data.find_all('tbody')
+    rows = table[2].find_all('tr')
     
     
     for row in rows:
         col = row.find_all('td')
         if len(col) != 0 :
             dicionario = {
-                "nome": col[0].get_text(strip=True),
-                "mc_usd_billion": col[2].get_text(strip=True)
+                "Name": col[0].get_text(strip=True),
+                "MC_USD_BILLION": col[2].get_text(strip=True)
             }
             df1 = pd.DataFrame(dicionario, index=[0])
             df = pd.concat([df, df1], ignore_index=True)
@@ -31,36 +31,36 @@ def extrair(url, atributos_tabela):
     
     
 
-def transformar(df):
+def transform(df):
     exchange_rates = pd.read_csv('exchange_rate.csv')
     rates_dict = exchange_rates.set_index('Currency').to_dict()['Rate']
-    gpb_rate = float(rates_dict['GBP'])
+    gbp_rate = float(rates_dict['GBP'])
     eur_rate = float(rates_dict['EUR'])
     inr_rate = float(rates_dict['INR'])
     
-    df['mc_usd_billion'] = df['mc_usd_billion'].astype(str).str.replace(',', '')
-    df['mc_usd_billion'] = pd.to_numeric(df['mc_usd_billion'], errors='coerce')
+    df['MC_USD_BILLION'] = df['MC_USD_BILLION'].astype(str).str.replace(',', '')
+    df['MC_USD_BILLION'] = pd.to_numeric(df['MC_USD_BILLION'], errors='coerce')
     
-    df['mc_gbp_billion'] = np.round(df['mc_usd_billion'] * gpb_rate, 2)
-    df['mc_eur_billion'] = np.round(df['mc_usd_billion'] * eur_rate, 2)
-    df['mc_inr_billion'] = np.round(df['mc_usd_billion'] * inr_rate, 2)
+    df['MC_GBP_BILLION'] = [np.round(x * gbp_rate, 2) for x in df['MC_USD_BILLION']]
+    df['MC_EUR_BILLION'] = [np.round(x * eur_rate, 2) for x in df['MC_USD_BILLION']]
+    df['MC_INR_BILLION'] = [np.round(x * inr_rate, 2) for x in df['MC_USD_BILLION']]
     return df
     
     
-def carregar_csv(df, csv_path):
+def load_csv(df, csv_path):
     df.to_csv(csv_path)
     
-def carregar_sql(df, conexao_sql, nome_tabela):
-    df.to_sql(nome_tabela, conexao_sql, if_exists='replace', index=False)
+def load_sql(df, conn, table_name):
+    df.to_sql(table_name, conn, if_exists='replace', index=False)
     
     
-def query_statement(query, conexao_sql):
+def query_statement(query, conn):
     print(query)
-    query_output = pd.read_sql(query, conexao_sql)
+    query_output = pd.read_sql(query, conn)
     print(query_output)
     
 
-def log(message):
+def log_process(message):
     timestamp_format = '%Y-%g-%d-%H:%M:%S'
     now = datetime.now()
     timestamp = now.strftime(timestamp_format)
@@ -68,55 +68,52 @@ def log(message):
         f.write(timestamp + ': ' + message + '\n')
 
 
-log('Preliminares completas. Iniciando o processo ETL')
+log_process('Preliminaries complete. Initiating ETL process')
 
 
 query1 = f'SELECT * FROM Largest_banks'
 query2 = f'SELECT AVG(MC_GBP_Billion) FROM Largest_banks'
-query3 = f'SELECT nome from Largest_banks LIMIT 5'
+query3 = f'SELECT Name from Largest_banks LIMIT 5'
 
 
 url = 'https://en.wikipedia.org/wiki/List_of_largest_banks'
-atributos_tabela = ['nome', 'mc_usd_billion']
+table_attribs = ['Name', 'MC_USD_BILLION']
 
 
 csv_path = 'Largest_banks_data.csv'
 
 banco_bd = 'Banks.db'
 
-tabela = 'Largest_Banks'
+table = 'Largest_Banks'
 
 
-log('inicio de extracao')
-df = extrair(url, atributos_tabela)
-print(df.head())
+df = extract(url, table_attribs)
+print(df)
 
-log('fim de extracao')
+log_process('Data extraction complete. Initiating Transformation process')
 
+transform(df)
+print(df)
 
-log('inicio de transformacao')
+log_process('Data transformation complete. Initiating Loading process')
 
-transformar(df)
-print(df.head())
+load_csv(df, csv_path)
 
-log('Fim de transformacao')
+log_process('Data saved to CSV file')
 
-
-log('Dados salvos no arquivo CSV')
-
-carregar_csv(df, csv_path)
-
-log('Conexao SQL iniciada')
+log_process('SQL Connection initiated')
 
 sql_connection = sqlite3.connect('Banks.db')
 
-log('Dados carregados no Banco de Dados como uma tabela, Executando consultas ')
-carregar_sql(df, sql_connection, tabela)
 
-log('Processo Completo')
+load_sql(df, sql_connection, table)
+log_process('	Data loaded to Database as a table, Executing queries')
+
 query_statement(query1, sql_connection)
 query_statement(query2, sql_connection)
 query_statement(query3, sql_connection)
+log_process('	Process Complete')
 
-log('Conexao com o servidor fechada')
+
 sql_connection.close()
+log_process('Server Connection closed')
