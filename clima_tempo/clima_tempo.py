b23a185ab9c1861eebec6cd9418d1a81ed4adbdb
Jose Marcio <josemarciojunior10@gmail.com>
2026-05-07 22:08:44 -0300

    Processo de ETL consumindo dados de uma API de clima. Feito o processo de Extração e uma parte do Transform

---
+++ clima_tempo/clima_tempo.py
@@ -0,0 +1,54 @@
+import requests
+import pandas as pd
+import os
+from dotenv import load_dotenv
+load_dotenv()
+
+
+# Função para extrair os dados da API
+def extract(cidades, api_key):
+    resultados = []
+    
+    for cidade in cidades:
+        url = f'https://api.openweathermap.org/data/2.5/weather?q={cidade}&APPID={api_key}'
+        response = requests.get(url)
+        print(f'Cidade: {cidade}, Status Code: {response.status_code}')
+
+        if response.status_code == 200:
+           resultados.append(response.json())
+        else:
+            print(f'Aviso: falha ao buscar {cidade} - Status {response.status_code}')
+    return resultados
+
+
+def transform(dados_brutos):
+    registros = []
+    
+    for dado in dados_brutos:
+        registro = {
+            'cidade': dado['name'],
+            'temperatura': dado['main']['temp'],
+            'sensacao_termica': dado['main']['feels_like'],
+            'umidade': dado['main']['humidity'],
+            'pressao': dado['main']['pressure'],
+            'velo_vento': dado['wind']['speed'],
+            'desc_clima': dado['weather'][0]['description'],
+            'coletado_em': pd.Timestamp.now()
+        }
+        registros.append(registro)
+    return pd.DataFrame(registros)
+
+# Lista de Cidades para consulta
+cidades = ['Vitoria', 'São Paulo', 'Rio de Janeiro', 'Belo Horizonte']
+api_key = os.getenv('OPENWEATHER_API_KEY')
+
+#Inicia o processo de extração dos dados
+
+dados_brutos = extract(cidades, api_key)
+print (f'Total de cidades coletadas: {len(dados_brutos)}')
+
+    
+# Transformação dos dados
+
+DF = transform(dados_brutos)
+print(DF)
