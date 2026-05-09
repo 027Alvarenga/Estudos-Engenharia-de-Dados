a39459de3bc34499abd5092d8a768f2d84909652
Jose Marcio <josemarciojunior10@gmail.com>
2026-05-08 22:14:18 -0300

    Implementação da biblioteca logging para criar avisos do progresso do processo. Inserção da função run e main para executar somente diretamente pelo arquivo

--- clima_tempo/clima_tempo.py
+++ clima_tempo/clima_tempo.py
@@ -1,12 +1,17 @@
 import requests
 import pandas as pd
 import os
+import logging
 from dotenv import load_dotenv
 load_dotenv()
 from sqlalchemy import create_engine
 
 
-
+logging.basicConfig(
+    level=logging.INFO,
+    format='%(asctime)s - %(levelname)s - %(message)s'
+)
+logger = logging.getLogger(__name__)
 
 # Função para extrair os dados da API
 def extract(cidades, api_key):
@@ -15,12 +20,12 @@ def extract(cidades, api_key):
     for cidade in cidades:
         url = f'https://api.openweathermap.org/data/2.5/weather?q={cidade}&units=metric&lang=pt_br&APPID={api_key}'
         response = requests.get(url)
-        print(f'Cidade: {cidade}, Status Code: {response.status_code}')
+        logger.info(f'Cidade: {cidade}, Status Code: {response.status_code}')
 
         if response.status_code == 200:
            resultados.append(response.json())
         else:
-            print(f'Aviso: falha ao buscar {cidade} - Status {response.status_code}')
+            logger.warning(f'Falha ao buscar dados para {cidade} - Status {response.status_code}')
     return resultados
 
 
@@ -53,24 +58,27 @@ def load(df):
         index=False
     )
     
-    print(f'{len(df)} registros inseridos com sucesso!')
+    logger.info('Dados carregados com sucesso no Supabase')
 
 
+def run():
+    # Lista de Cidades para consulta
+    cidades = ['Vitoria', 'São Paulo', 'Rio de Janeiro', 'Belo Horizonte']
+    api_key = os.getenv('OPENWEATHER_API_KEY')
 
-# Lista de Cidades para consulta
-cidades = ['Vitoria', 'São Paulo', 'Rio de Janeiro', 'Belo Horizonte']
-api_key = os.getenv('OPENWEATHER_API_KEY')
+    #Inicia o processo de extração dos dados
 
-#Inicia o processo de extração dos dados
+    dados_brutos = extract(cidades, api_key)
+    logger.info(f'Total de cidades coletadas: {len(dados_brutos)}')
 
-dados_brutos = extract(cidades, api_key)
-print (f'Total de cidades coletadas: {len(dados_brutos)}')
+    # Transformação dos dados
 
-# Transformação dos dados
+    DF = transform(dados_brutos)
+    logger.info('Transformação dos dados concluída com sucesso')
 
-DF = transform(dados_brutos)
-print(DF)
-print(DF.dtypes)
-
-# Carregamento no Supabase
-load(DF)
+    # Carregamento no Supabase
+    load(DF)
+    logger.info('Processo ETL concluído com sucesso')
+    
+if __name__ == '__main__':
+    run()
